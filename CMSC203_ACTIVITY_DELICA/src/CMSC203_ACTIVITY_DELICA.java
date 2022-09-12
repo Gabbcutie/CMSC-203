d4c3cfbb57b327f451daacd9b4c071f6712ae3e5
Gabriel Delica <113229092+Gabbcutie@users.noreply.github.com>
2022-09-12 17:36:21 +0800

    Add files via upload

---
+++ CMSC203_ACTIVITY_DELICA/src/CMSC203_ACTIVITY_DELICA.java
@@ -0,0 +1,9 @@
+public class CMSC203_ACTIVITY_DELICA {
+    public static void main(String[] args) {
+
+        System.out.println("Hi, my name is Gabriel Delica");
+        System.out.println("I am 22 years old");
+        System.out.println("My GPA is 1.25");
+        System.out.println("My blood type is A");
+    }
+}
