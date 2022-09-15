0e668ac59ff531bda9283311eb2dbc53639b6214
Gabriel Delica <113229092+Gabbcutie@users.noreply.github.com>
2022-09-15 18:14:11 +0800

    Update CMSC203_ENGAGINGACTIVITY_DELICA.cs

--- CMSC203_ENGAGINGACTIVITY_DELICA.cs
+++ CMSC203_ENGAGINGACTIVITY_DELICA.cs
@@ -1,26 +1,26 @@
 public class ProgramStruct {
     struct Employee{
-        public string firstName;
-        public string lastName;
-        public int age;
-        public float CompletedTraining;
-    }
-    public static void Main(string[] args){
 
+    public string firstName;
+    public string lastName;
+    public int age;
+    public string completedTraining;
+}
+class Program{
+    public static void Main(string[] args)
+    {
+        Console.Title = "Stuct";
         Employee employee1;
+        employee1.firstName = "Juan";
+        employee1.lastName = "Dela Cruz";
+        employee1.age = 25;
+        employee1.completedTraining = "Yes";
 
-        Console.WriteLine("What is your first name?");
-        employee1.firstName = Console.ReadLine( );
-        Console.WriteLine("What is your last name?");
-        employee1.lastName = Console.ReadLine( );
-        Console.WriteLine("What is your age?");
-        employee1.age = int.Parse(Console.ReadLine( ));
-        Console.WriteLine("Do you completed the training?");
-        employee1.CompletedTraining = float.Parse(Console.ReadLine( ));
-        Console.WriteLine("You entered");
-        Console.WriteLine("firstName: "+employee1.firstName);
-        Console.WriteLine("lastName: "+employee1.lastName);
-        Console.WriteLine("age: "+employee1.age);
-        Console.WriteLine("Completed Training: "+employee1.CompletedTraining);
-        }
+        Console.WriteLine("Name: "+employee1.firstName);
+        Console.WriteLine("Last Name: "+employee1.lastName);
+        Console.WriteLine("Age: "+employee1.age);
+        Console.WriteLine("Completed Training: "+employee1.completedTraining);
+        Console.Read();
+
+    }
 }
