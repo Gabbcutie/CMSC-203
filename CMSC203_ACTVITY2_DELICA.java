fd4201cee5dca146188c16acd64bafd6d50aa381
Gabriel Delica <113229092+Gabbcutie@users.noreply.github.com>
2022-09-16 17:19:43 +0800

    Add files via upload

---
+++ CMSC203_ACTVITY2_DELICA.java
@@ -0,0 +1,11 @@
+
+public class Test{
+public static void main(String[] args){
+ int number;
+ 
+ for(number = 7; number >= 5 number ++){
+ int answer = number + 5
+ System.out.printIn(number + "plus 5 equals" + answer);
+  }
+ }
+} 
